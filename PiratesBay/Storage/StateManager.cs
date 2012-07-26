09a7711599398078888a2c3647f21bd5aa770c41
Anton Sizikov <anton.sizikov@studiomobile.ru>
2012-07-26 23:44:11 +0400

    State manager adde

---
+++ PiratesBay/Storage/StateManager.cs
@@ -0,0 +1,19 @@
+﻿using System.IO.IsolatedStorage;
+
+namespace PiratesBay.Storage
+{
+    public class StateManager
+    {
+        private IsolatedStorageFile _storage;
+        
+        public StateManager()
+        {
+            _storage = IsolatedStorageFile.GetUserStoreForApplication();
+        }
+
+        public bool HasSavedState
+        {
+            get { return false; }
+        }
+    }
+}
