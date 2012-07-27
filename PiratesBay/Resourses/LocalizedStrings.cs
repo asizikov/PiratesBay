050084555fa2a371be152cba1ec5737c3b35ab0b
Anton Sizikov <asizikov@spbtv.com>
2012-07-27 11:51:17 +0400

    UI Changes
    Bindable AppBar, resources with English and Russian strings added

---
+++ PiratesBay/Resourses/LocalizedStrings.cs
@@ -0,0 +1,20 @@
+﻿using PiratesBay.Resourses;
+
+namespace PiratesBay
+{
+    public class LocalizedStrings
+    {
+
+       public LocalizedStrings() { }
+
+        private static readonly Strings strings = new Strings();
+
+        public Strings Strings
+        {
+            get
+            {
+                return strings;
+            }
+        }
+    }
+}
