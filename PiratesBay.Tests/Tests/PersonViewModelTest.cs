387fbd7bbe9e46fc7ea30200d7457fba401dd13f
Anton Sizikov <anton.sizikov@studiomobile.ru>
2012-07-29 18:40:17 +0400

    PersonViewModel test updated
    broken code removed

--- PiratesBay.Tests/Tests/PersonViewModelTest.cs
+++ PiratesBay.Tests/Tests/PersonViewModelTest.cs
@@ -1,35 +1,20 @@
 ﻿using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PiratesBay.Domain;
-using PiratesBay.ViewModels;
+using PiratesBay.Model;
 
 namespace PiratesBay.Test.Unit
 {
     [TestClass]
     public class PersonViewModelTest
     {
-        [TestInitialize]
-        public void SetUp()
-        {
-            ;
-        }
-
-        [TestMethod]
-        public void InitTest()
-        {
-            var person = new MoqPerson(10.0);
-            var personVM = new PersonViewModel(person);
-            Assert.IsNotNull(personVM);
-        }
 
         [TestMethod]
         public void ViewModelPropertiesAreEqualToDomainPropertiesTest()
         {
-            var personsList = new List<IPerson>();
-            var personVM = new PersonViewModel();
-            personVM.Name = "Anton";
-            personVM.Spent = 10.0;
-            personVM.Debt = 0.0;
+            var personsList = new List<Person>();
+            var personVM = new Person 
+            {Name = "Anton", Spent = 10.0, Debt = 0.0};
             Assert.AreEqual("Anton", personVM.Name);
             Assert.AreEqual(10.0, personVM.Spent);
             Assert.AreEqual(0.0, personVM.Debt);
@@ -37,7 +22,7 @@ namespace PiratesBay.Test.Unit
             var eventRaiseCounter = 0;
             personVM.PropertyChanged += (s,arg) => eventRaiseCounter++;
 
-            var otherPerson = new MoqPerson(20.0);
+            var otherPerson = new Person { Spent = 20.0 };
             var calculation = new Calculation(personsList);
             personsList.Add(personVM);
             personsList.Add(otherPerson);
