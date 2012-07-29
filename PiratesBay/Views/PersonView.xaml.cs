7e38193cc5d0b7fc9b2ea98b8f88e333e91f6d4b
Anton Sizikov <anton.sizikov@studiomobile.ru>
2012-07-29 18:39:40 +0400

    Checkbox added to Personview. layout updated

---
+++ PiratesBay/Views/PersonView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace PiratesBay.Views
+{
+    public partial class PersonView : UserControl
+    {
+        public PersonView()
+        {
+            InitializeComponent();
+        }
+    }
+}
